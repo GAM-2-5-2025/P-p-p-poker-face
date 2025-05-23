39583aa7d0ca8696b289c7720b920806bbe2dc2c
Sagnjin <vlasicsanjin@gmail.com>
2025-05-23 10:41:10 +0200

    Nebitno
    Spremio poziciju miša i pomakao dio koda vezan za miš

--- python_test.py
+++ python_test.py
@@ -12,12 +12,14 @@ class App:
         self._display_surf = pygame.display.set_mode((700,700), pygame.HWSURFACE)
         self._running = True
         self._image_surf = pygame.image.load("ba33e6f8-5790-4f75-bed1-96a76d8e06ce.jpg").convert()
+        pygame.mouse.set_visible(True)
  
     def on_event(self, event):
         if event.type == QUIT:
             self._running = False
     def on_loop(self):
-        pass
+        mouse = pygame.mouse.get_pos()
+        
     def on_render(self):
         self._display_surf.blit(self._image_surf,(0,0))
         pygame.display.flip()
@@ -29,8 +31,7 @@ class App:
         if self.on_init() == False:
             self._running = False
  
-        while( self._running ):
-         pygame.mouse.set_visible(True)
+        while( self._running )
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
